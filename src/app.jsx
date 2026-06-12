87ea41c9b4712c7895666b3e57e8aae3c39091e6
Ishant921 <84021307+Ishant921@users.noreply.github.com>
2026-06-12 18:28:58 +0530

    Simplify App component to use HexGrid
    Refactor App component to render HexGrid directly.

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,5 @@
+import HexGrid from "./HexGrid.jsx";
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app.jsx";
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-  React.createElement(App)
-);
+export default function App() {
+  return <HexGrid />;
+}
